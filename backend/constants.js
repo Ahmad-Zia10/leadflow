0130db0baa2a4bb9dc9617eb2506f003f65f658b
Ahmad-Zia10 <mail.ahmadzia07@gmail.com>
2026-05-12 01:41:16 +0530

    add lead status constants

---
+++ backend/constants.js
@@ -0,0 +1,12 @@
+export const LEAD_STATUS = {
+    NEW: "New",
+    CONTACTED: "Contacted",
+    QUALIFIED: "Qualified",
+    PROPOSAL_SENT: "Proposal Sent",
+    WON: "Won",
+    LOST: "Lost"
+}
+
+export const LEAD_STATUS_VALUES = Object.values(LEAD_STATUS);
+
+export const DB_NAME = "leadflow"
