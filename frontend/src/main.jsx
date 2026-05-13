b51f9332594f55003d32037cdba6355f3eb6e57c
Ahmad-Zia10 <mail.ahmadzia07@gmail.com>
2026-05-13 15:48:57 +0530

    setup main.jsx with Redux and React Query providers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import store from "./store/store"
+import App from "./App"
+import "./index.css"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1,
+            staleTime: 30 * 1000, // 30 seconds
+        }
+    }
+})
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </Provider>
+    </StrictMode>
 )
